8948d5bad5e8e547c653b9286d2926577c20bd7b
SUMIN97 <julia4034280@naver.com>
2020-11-18 15:31:23 +0900

    update:AlignCollate parameters

--- train.py
+++ train.py
@@ -9,25 +9,22 @@ import torch
 from torchvision.models import resnet34
 
 from dataset import SegDataset, AlignCollate
+from setting import Settings
 
+settings = Settings()
 train_dataset = SegDataset()
 assert train_dataset
 
 train_align_collate = AlignCollate(
     'training',
-    ts.N_CLASSES,
-    ts.MAX_N_OBJECTS,
-    ts.MEAN,
-    ts.STD,
-    ts.IMAGE_HEIGHT,
-    ts.IMAGE_WIDTH,
-    random_hor_flipping=ts.HORIZONTAL_FLIPPING,
-    random_ver_flipping=ts.VERTICAL_FLIPPING,
-    random_transposing=ts.TRANSPOSING,
-    random_90x_rotation=ts.ROTATION_90X,
-    random_rotation=ts.ROTATION,
-    random_color_jittering=ts.COLOR_JITTERING,
-    random_grayscaling=ts.GRAYSCALING,
-    random_channel_swapping=ts.CHANNEL_SWAPPING,
-    random_gamma=ts.GAMMA_ADJUSTMENT,
-    random_resolution=ts.RESOLUTION_DEGRADING)
+    settings.N_CLASSES,
+    settings.MAX_N_OBJECTS,
+    settings.MEAN,
+    settings.STD,
+    settings.IMAGE_HEIGHT,
+    settings.IMAGE_WIDTH)
+
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size = 8,
+                                           shuffle = True,
+                                           collate_fn = train_align_collate)
